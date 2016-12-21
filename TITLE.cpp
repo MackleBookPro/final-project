1802b03b3f2ca2864657342d907411ccd01278c5
Michael Schwartz <macklebookpro@gmail.com>
2016-12-21 09:23:11 -0500

    Add files via upload

---
+++ TITLE.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+enum colour { DARKBLUE = 1, DARKGREEN, DARKTEAL, DARKPINK, DARKRED, DARKYELLOW, GRAY, DARKGRAY, BLUE, GREEN, TEAL, RED, PINK, YELLOW, WHITE };
+
+struct setcolour
+{
+   colour _c;
+   HANDLE _console_handle;
+
+
+       setcolour(colour c, HANDLE console_handle)
+           : _c(c), _console_handle(0)
+       {
+           _console_handle = console_handle;
+       }
+};
+
+// We could use a template here, making it more generic. Wide streams won't
+// work with this version.
+basic_ostream<char> &operator<<(basic_ostream<char> &s, const setcolour &ref)
+{
+    SetConsoleTextAttribute(ref._console_handle, ref._c);
+    return s;
+}
+
+int main(int argc, char *argv[])
+{
+    HANDLE chandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    cout << setcolour(DARKTEAL, chandle) << ".::   .::                                                        .::" << endl;
+    cout << setcolour(DARKTEAL, chandle) << ".::  .::                                                         .::" << endl;
+    cout << setcolour(DARKTEAL, chandle) << ".:: .::       .::    .::   .:: .::::        .::    .:: .::       .::" << endl;
+    cout << setcolour(DARKTEAL, chandle) << ".: .:       .:   .::  .:: .:: .::         .::  .::  .::  .:: .:: .::" << endl;
+    cout << setcolour(DARKTEAL, chandle) << ".::  .::   .::::: .::   .:::    .:::     .::   .::  .::  .::.:   .::" << endl;
+    cout << setcolour(DARKTEAL, chandle) << ".::   .::  .:            .::      .::    .::   .::  .::  .::.:   .::" << endl;
+    cout << setcolour(DARKTEAL, chandle) << ".::     .::  .::::      .::   .:: .::      .:: .:::.:::  .:: .:: .::" << endl;
+    cout << setcolour(DARKTEAL, chandle) << "                      .::                                        " << endl;
+    cout << setcolour(DARKYELLOW, chandle) << "    .::::                               .::                          " << endl;
+    cout << setcolour(DARKYELLOW, chandle) << "  .::    .::                            .::                          " << endl;
+    cout << setcolour(DARKYELLOW, chandle) << ".::       .::.::  .::   .::    .: .:::.:.: .:   .::    .: .::: .:::: " << endl;
+    cout << setcolour(DARKYELLOW, chandle) << ".::       .::.::  .:: .::  .::  .::     .::   .:   .::  .::   .::    " << endl;
+    cout << setcolour(DARKYELLOW, chandle) << ".::       .::.::  .::.::   .::  .::     .::  .::::: .:: .::     .::: " << endl;
+    cout << setcolour(DARKYELLOW, chandle) << "  .:: .: .:: .::  .::.::   .::  .::     .::  .:         .::       .::" << endl;
+    cout << setcolour(DARKYELLOW, chandle) << "    .:: ::     .::.::  .:: .:::.:::      .::   .::::   .:::   .:: .::" << endl;
+    cout << setcolour(DARKYELLOW, chandle) << "         .:                                                          " << endl;
+
+
+
+
+
+
+    cin.get();
+}
